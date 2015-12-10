63f84e6003cc78f89a612f85cae967a1e82afe98
eveningkid <arn.dellinger@gmail.com>
2015-12-10 16:06:18 +0100

    WYSLSW: new reasons added

--- whyyoushouldlovestarwars/assets/js/reasons.js
+++ whyyoushouldlovestarwars/assets/js/reasons.js
@@ -18,8 +18,44 @@ var reasons = [
 		img: "https://media.giphy.com/media/K9yzeKyvvva9i/giphy.gif",
 		text: "sometimes Vader will make you smile. At least, after what he became."
 	},{
-		img: "https://media.giphy.com/media/YjCNKPkea0QjS/giphy.gif",
+		img: "https://media.giphy.com/media/ns8SCo6O6g7nO/giphy.gif",
 		text: "you'll make new friends and...wait, wait, how cute is that? I'm sure it melted your heart, don't lie to me."
+	},{
+		img: "https://media.giphy.com/media/KsMMDP7yokx2w/giphy.gif",
+		text: "Vader can be cute af. Look at him."
+	},{
+		img: "https://media.giphy.com/media/3oEduFNHfDMjyIpgCk/giphy.gif",
+		text: "Drake dance moves approved it."
+	},{
+		img: "https://media.giphy.com/media/mT1SJO4ZXMKE8/giphy.gif",
+		text: "Sand People will teach you how to party. Hope you're ready."
+	},{
+		img: "https://media.giphy.com/media/xTiTnDCzB5QjCb9GRq/giphy.gif",
+		text: "you'll finally understand this part of the trailer that makes us crying."
+	},{
+		img: "https://media.giphy.com/media/3o85xmS7ZGEAC8H2U0/giphy.gif",
+		text: "BOOM, BOOM, BOOM! Yeah baby, now we're talkin'! (hey, it's also about the story)"
+	},{
+		img: "https://media.giphy.com/media/VyqJ0E6oHInAs/giphy.gif",
+		text: "this. is. cool."
+	},{
+		img: "https://media.giphy.com/media/2FazrCNDkgIqFVFsc/giphy.gif",
+		text: "Chewbacca loves baseball too."
+	},{
+		img: "https://media.giphy.com/media/X50f9uthkjN60/giphy.gif",
+		text: "evil guys are evil. And really badass."
+	},{
+		img: "https://media.giphy.com/media/13NR9a0aYuYMy4/giphy.gif",
+		text: "you'll never see 3D animation as before."
+	},{
+		img: "https://media.giphy.com/media/e6e1P3wC6xkYg/giphy.gif",
+		text: "you'll laugh. I dare you not to smile to this one."
+	},{
+		img: "https://media.giphy.com/media/2lyoAomLROMN2/giphy.gif",
+		text: "we won't look at you again like this when you say 'I've never watched Star Wars'."
+	},{
+		img: "https://media.giphy.com/media/r3knno4y8phhC/giphy.gif",
+		text: "Yoda is damn too funny in the first episodes."
 	}
 ];
 
