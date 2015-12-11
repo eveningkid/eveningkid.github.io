5964c6363a6ee8d495934268eef5c417b1785daf
Arnaud <eveningkid@users.noreply.github.com>
2015-12-11 11:35:56 +0100

    WYSLSW: Not redisplay twice a reason

--- whyyoushouldlovestarwars/assets/js/app.js
+++ whyyoushouldlovestarwars/assets/js/app.js
@@ -6,32 +6,45 @@ var reason = getReason();
 
 document.querySelector('#wrapper').style.height = window.innerHeight + 'px';
 
+/**
+ * Return a reason
+ * @return {object} val - reason object
+ */
+
 function getReason () {
-	return reasons[
-		Math.round(Math.random() * (reasons.length - 1))
-	];
+
+	var i   = Math.round(Math.random() * (reasons.length - 1)),
+	    val = reasons[i];
+
+	reasons.splice(i, 1);
+
+	return val || {
+		img: "https://media.giphy.com/media/pDTCmXCVXoMCY/giphy.gif",
+		text: "Because there is no other reason for now. Why don't give yours on Github?"
+	};
+
 }
+
+/**
+ * Display one reason (text and picture)
+ * @param {boolean} lazyload - true, if we lazyload the picture 
+ * @param {boolean} first - true, if first call
+ */
 
 function gimme (lazyload, first) {
 
-	if (reasons.length) {
+	var main      = document.querySelector('#main'),
+	    container = document.querySelector('#reason-view');
 
-		var main      = document.querySelector('#main'),
-		    container = document.querySelector('#reason-view');
+	if (!lazyload) {
+		if (!first)	reason = getReason();
 
-		if (!lazyload) {
-			if (!first)	reason = getReason();
-
-			main.style.display = 'none';
-			container.style.display = 'block';
-		}
-
-		container.querySelector('img').src = reason.img;
-		container.querySelector('p').innerHTML = reason.text;
-
-	} else {
-		console.info("Oh, apparently, the reasons are not loaded yet. Please, keep waiting.");
+		main.style.display = 'none';
+		container.style.display = 'block';
 	}
+
+	container.querySelector('img').src = reason.img;
+	container.querySelector('p').innerHTML = reason.text;
 
 }
 
