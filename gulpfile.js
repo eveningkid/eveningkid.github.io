acf24fc994c74f1d9ab9043b3a44bf419ccc5a7e
eveningkid <arn.dellinger@gmail.com>
2018-05-14 01:24:20 +0800

    Upgraded gulp, updated gulp code

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,21 @@
-const gulp = require('gulp')
-const sass = require('gulp-sass')
+const gulp = require('gulp');
+const sass = require('gulp-sass');
 
 const paths = {
-  scss: './scss/**/*.scss',
-}
+  style: {
+    from: './scss/**/*.scss',
+    to: './assets/css',
+  },
+};
 
 gulp.task('sass', function () {
-  return gulp.src(paths.scss)
-    .pipe(sass({outputStyle: 'compressed'}))
-    .pipe(gulp.dest('./assets/css'))
-})
+  return gulp.src(paths.style.from)
+    .pipe(sass({ outputStyle: 'compressed' }))
+    .pipe(gulp.dest(paths.style.to));
+});
 
 gulp.task('watch', function () {
-  gulp.watch(paths.scss, ['sass'])
-})
+  gulp.watch(paths.style.from, gulp.series('sass'));
+});
 
-gulp.task('default', ['watch'])
+gulp.task('default', gulp.series('watch'));
